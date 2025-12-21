6c560093fa72091b8709d672bd3fb3c2c9c0b4e6
kevin26720 <kevin.villacis03@epn.edu.ec>
2025-12-21 16:34:35 -0500

    lab 06 - RecyclerView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.security.crypto)
     implementation(libs.security.crypto)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/villacis/kevin/cazarpatos/Player.kt
@@ -0,0 +1,6 @@
+package com.villacis.kevin.cazarpatos
+
+data class Player (var username:String, var huntedDucks:Int){
+    constructor():this("",0)
+}
+
